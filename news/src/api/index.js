c6b1267ba1cab3ef6fc4dff62dcb6e8b2b91291c
amazingrace17 <78857611+amazingrace17@users.noreply.github.com>
2021-12-10 13:08:45 +0100

    secure

--- news/src/api/index.js
+++ news/src/api/index.js
@@ -8,7 +8,7 @@ export const fetchSources = async () => {
     // const requestSource = `sources?apiKey=`;
     try {
         
-        const { data: { title } } = await axios.get(`http://api.mediastack.com/v1/news?access_key=b825f2dad23816419b700bbf07aa6a0a&categories=general&countries=ni`);
+        const { data: { title } } = await axios.get(`https://api.mediastack.com/v1/news?access_key=b825f2dad23816419b700bbf07aa6a0a&categories=general&countries=ni`);
         
   
         return title.map((titl)=> titl);
